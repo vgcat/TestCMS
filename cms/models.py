207a991dbbf6f5ece1fb1844c54f75e4fbde8fd3
vgcat <anna.lysytska@jelvix.com>
2018-11-15 18:54:04 +0200

    11

--- cms/models.py
+++ cms/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
-from .manage import UserManager
+from .managers import UserManager
 
 from django.utils.translation import ugettext_lazy as _
 # Create your models here.
